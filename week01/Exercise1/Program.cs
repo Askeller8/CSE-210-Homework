dcd56dc40b4cecc251531d808769163159e0f4d0
Scott Keller <Scott8Keller@gmail.com>
2026-05-14 17:09:49 -0400

    Completed the Week01 Exercise 1 project

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your First name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your Last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"your name is {lastName}, {firstName} {lastName}");
     }
 }
